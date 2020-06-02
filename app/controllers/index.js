c88a2fd1310fa153d11d860bab5fcd0531636fbf
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-06-02 18:19:10 +0200

    Fix per Android, build APK da distribuire

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,3 @@
 tabgroup = $.index;
-var actionBar;
-if (Ti.Platform.osname === "android") {
-		if (! $.tab.activity) {
-			 Ti.API.error("No action bar.");
-		} else {
-				actionBar = $.tab.getActivity().actionBar;
-				if (actionBar) {
-						actionBar.setTitle($.tab.title);
-				}
-		}
-}
 
 $.index.open();
