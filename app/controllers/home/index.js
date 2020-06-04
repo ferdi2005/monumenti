c89d7a7425d6eaa5cd045595f2af62f0c5db3690
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-06-04 18:21:25 +0200

    Fix esecuzione della function locate()

--- app/controllers/home/index.js
+++ app/controllers/home/index.js
@@ -4,83 +4,86 @@ const Map = require('ti.map');
 // $.activityIndicator.style = Titanium.UI.ActivityIndicatorStyle.DARK;
 $.errorlabel.hide();
 function findmon(e) {
-  if(Ti.Geolocation.locationServicesEnabled) {
-  $.mapview.removeAllCircles();
+  if (Ti.Geolocation.locationServicesEnabled) {
+    $.mapview.removeAllCircles();
     var args = e;
     lat = args.coords.latitude;
     lon = args.coords.longitude;
     url = 'https://wlm.puglia.wiki/monuments.json?latitude=' + lat + '&longitude=' + lon;
-   // $.activityIndicator.show();
+    // $.activityIndicator.show();
     var circle = $.mapview.addCircle(Map.createCircle({
       radius: 50,
       center: [e.coords.longitude, e.coords.latitude],
       fillColor: "#4289ef"
-    }));    
+    }));
 
-  var xhr = Ti.Network.createHTTPClient({
-        onload: function(e) {
-          var response = JSON.parse(this.responseText);
-          $.mapview.removeAllAnnotations();
-                $.mapview.region = {
-                  latitude: response[1][0],
-                  longitude: response[1][1], 
-                  latitudeDelta: 0.1,
-                   longitudeDelta: 0.1,
-                   zoomLevel: 10
-              };
-              $.mapview.center = [args.coords.latitude, args.coords.longitude];
+    var xhr = Ti.Network.createHTTPClient({
+      onload: function (e) {
+        var response = JSON.parse(this.responseText);
+        $.mapview.removeAllAnnotations();
+        $.mapview.region = {
+          latitude: response[1][0],
+          longitude: response[1][1],
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+          zoomLevel: 10
+        };
+        $.mapview.center = [args.coords.latitude, args.coords.longitude];
 
-              $.mapview.mapType = Map.NORMAL_TYPE;
-              $.mapview.height = Ti.UI.FILL;
-            response[0].forEach(function(item)  {
-              annotation = Map.createAnnotation({
-                latitude: item.latitude,
-                longitude: item.longitude,
-                title: item.itemLabel,
-                pincolor: Map.ANNOTATION_RED,
-                myid: item.id
-                });
-                $.mapview.addAnnotation(annotation);
-              });
-    //  $.activityIndicator.hide();
-        },
-        onerror: function(e) {
-          alert('Errore di connessione: ' + e.error);
-          // $.activityIndicator.hide();
+        $.mapview.mapType = Map.NORMAL_TYPE;
+        $.mapview.height = Ti.UI.FILL;
+        response[0].forEach(function (item) {
+          annotation = Map.createAnnotation({
+            latitude: item.latitude,
+            longitude: item.longitude,
+            title: item.itemLabel,
+            pincolor: Map.ANNOTATION_RED,
+            myid: item.id
+          });
+          $.mapview.addAnnotation(annotation);
+        });
+        //  $.activityIndicator.hide();
+      },
+      onerror: function (e) {
+        alert('Errore di connessione: ' + e.error);
+        // $.activityIndicator.hide();
 
-        },
-        timeout: 15000 // milliseconds
-      });
-      xhr.open('GET', url);
-        xhr.send();
-    } else {
-      alert('Abilita prima i servizi di localizzazione!');
-    }
-        
+      },
+      timeout: 15000 // milliseconds
+    });
+    xhr.open('GET', url);
+    xhr.send();
+  } else {
+    alert('Abilita prima i servizi di localizzazione!');
   }
 
-  if (Ti.Geolocation.hasLocationPermissions(Titanium.Geolocation.AUTHORIZATION_WHEN_IN_USE)) { 
-    Ti.Geolocation.getCurrentPosition(function(e) {
+}
+
+// TODO: Usare l'event listner location
+function locate() {
+  if (Ti.Geolocation.hasLocationPermissions(Titanium.Geolocation.AUTHORIZATION_WHEN_IN_USE)) {
+    Ti.Geolocation.getCurrentPosition(function (e) {
       findmon(e);
     });
-  } else {
-    Ti.Geolocation.requestLocationPermissions(Ti.Geolocation.AUTHORIZATION_WHEN_IN_USE, function(e) {
-      if(e.success) {
-      Ti.Geolocation.getCurrentPosition(function(e) {
-        findmon(e);
-      });
-    } else {
-      $.errorlabel.show();
-      $.mapview.region = {
-        latitude: 41.9109,
-        longitude: 12.4818, 
-        latitudeDelta: 0.1,
-         longitudeDelta: 0.1,
-         zoomLevel: 10
-    };
-    $.mapview.mapType = Map.NORMAL_TYPE;
-    $.mapview.height = Ti.UI.SIZE;
-    }
+  } else {
+    Ti.Geolocation.requestLocationPermissions(Ti.Geolocation.AUTHORIZATION_WHEN_IN_USE, function (e) {
+      if (e.success) {
+        Ti.Geolocation.getCurrentPosition(function (e) {
+          findmon(e);
+        });
+      } else {
+        $.errorlabel.show();
+        $.mapview.region = {
+          latitude: 41.9109,
+          longitude: 12.4818,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+          zoomLevel: 10
+        };
+        $.mapview.mapType = Map.NORMAL_TYPE;
+        $.mapview.height = Ti.UI.SIZE;
+      }
     });
-  }
+}}
 // setInterval(localize(), 120000);
+$.winmap.addEventListener('open', locate);
