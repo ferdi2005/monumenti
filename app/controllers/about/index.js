e72763df6504269f783fac12ce686c26d315ea26
Restyled.io <commits@restyled.io>
2020-06-08 15:06:47 +0000

    Restyled by clang-format

--- app/controllers/about/index.js
+++ app/controllers/about/index.js
@@ -1,8 +1,10 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+// Arguments passed into this controller can be accessed via the `$.args` object
+// directly or:
 var args = $.args;
 
 $.lbl_version.text = Ti.App.version;
 
-$.info.addEventListener("click", function(e){
-    Ti.Platform.openURL("https://wikilovesmonuments.wikimedia.it");
-});
+$.info.addEventListener(
+    "click",
+    function(
+        e) { Ti.Platform.openURL("https://wikilovesmonuments.wikimedia.it"); });
