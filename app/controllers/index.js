a73cd0ba4e510813c4c307f41998be4b0b81e1e8
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-07-13 16:07:49 +0200

    Bump ad 1.2; avviso versione non aggiornata

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,18 @@
 global.tabgroup = $.index;
-    
+
+var url = 'https://api.github.com/repos/ferdi2005/monumenti/releases/latest';
+var xhr = Ti.Network.createHTTPClient({
+    onload: function(e) {
+        response = JSON.parse(this.responseText);
+        if (response.tag_name != 'v' + Ti.App.version ) {
+            alert("Attenzione! Stai usando una versione non aggiornata dell'applicazione (" + Ti.App.version + ") vai sullo store a scaricare la nuova versione " + response.tag_name + ".");
+        }
+    },
+    onerror: function(e) {
+    },
+    timeout: 5000
+});
+xhr.open('GET', url);
+xhr.send();
+
 $.index.open();
