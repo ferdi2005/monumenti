10161bac756e2ced1d8a3a01cb29e400a8e9e3ff
ferdi2005 <ferdi.traversa@gmail.com>
2021-07-06 22:55:17 +0200

    Miglioramento del sistema di inserimento dati foto

--- app/controllers/upload/title.js
+++ app/controllers/upload/title.js
@@ -30,51 +30,118 @@ Array(images)[0].forEach(
                     Alloy.Globals.utils.open('upload/config', false);
                     return false;
                 } else if(JSON.parse(this.responseText).error == "Photo upload not succedeed.") {
-                    itemdata =  { 
-                        photo: {
-                            image: photo[0].media
-                        },
-                        template: "error",
-                    }
-                    data.push(itemdata);
-                    $.imagespace.setItems(data);
+                    var row = Titanium.UI.createTableViewRow({layout: "horizontal"});
+                    var image = Titanium.UI.createImageView({
+                        image: photo.media,
+                        top: "5dp",
+                        left: "5dp",
+                        width: "100dp",
+                        left: 0
+                    });
+
+                    var label = Titanium.UI.createLabel({
+                        text: "Errore nel caricamento di quest'immagine, riprova più tardi."
+                    });
+
+                    row.add(image);
+                    row.add(label);
+
+                    $.imagespace.add(row);
                 } else {
                     var id = JSON.parse(this.responseText).id;
-                    itemdata =  { 
-                        title: {
-                            id: "title" + id,
-                        },
-                        commonsdescription: {
-                            id: "description" + id,
-                        },
-                        date: {
-                            id: "date" + id,
-                        },
-                        photo: {
-                            image: photo.media
-                        },
-                        properties: {
-                            itemId: id,
-                        },
-                        template: "template",
-                    }
-                    data.push(itemdata); // Array delle cose da mostrare nel listitem
-                    uploaded.push(itemdata); // Array delle immagini caricate correttamente
-                    $.imagespace.setItems(data);
+                    var row = Titanium.UI.createTableViewRow({layout: "horizontal"});
+                    var image = Titanium.UI.createImageView({
+                        image: photo.media,
+                        top: "5dp",
+                        left: "5dp",
+                        width: "100dp",
+                        left: 0
+                    });
+
+                    var view = Titanium.UI.createView({
+                        layout: "vertical"
+                    });
+
+                    var title = Titanium.UI.createTextField({
+                        hintText: "Titolo dell'immagine",
+                        id: "title" + id,
+                        inputType: Titanium.UI.INPUT_TYPE_CLASS_TEXT,
+                        width: Ti.UI.FILL,
+                        left: "5dp",
+                        borderStyle: Ti.UI.INPUT_BORDERSTYLE_LINE,
+                        hintTextColor: '#A0A0A0',
+                        color: 'black',
+                        borderColor: 'black',
+                        borderWidth: '0.3'                    
+                    });
+                    
+                    fieldtext["title" + id] = title;
+
+                    view.add(title);
+
+                    var description = Titanium.UI.createTextField({
+                        hintText: "Descrizione dell'immagine",
+                        id: "description" + id,
+                        inputType: Titanium.UI.INPUT_TYPE_CLASS_TEXT,
+                        width: Ti.UI.FILL,
+                        left: "5dp",
+                        borderStyle: Ti.UI.INPUT_BORDERSTYLE_LINE,
+                        hintTextColor: '#A0A0A0',
+                        color: 'black',
+                        borderColor: 'black',
+                        borderWidth: '0.3'                    
+                    });
+
+                    fieldtext["description" + id] = description;
+
+                    view.add(description);
+
+                    var date = Titanium.UI.createTextField({
+                        hintText: "Data della foto (gg/mm/aaaa)",
+                        id: "date" + id,
+                        inputType: Titanium.UI.INPUT_TYPE_CLASS_TEXT,
+                        width: Ti.UI.FILL,
+                        left: "5dp",
+                        borderStyle: Ti.UI.INPUT_BORDERSTYLE_LINE,
+                        hintTextColor: '#A0A0A0',
+                        color: 'black',
+                        borderColor: 'black',
+                        borderWidth: '0.3'                    
+                    });
+
+                    fieldtext["date" + id] = date;
+
+                    view.add(date);
+
+                    row.add(image);
+                    row.add(view);
+                    uploaded.push(id); // Array delle immagini caricate correttamente
+
+                    $.imagespace.add(row);
                     $.activityIndicator.hide();
                     $.activityIndicator.height = 0;
                     $.conferma.show();
                 }
             },
             onerror: function(e) {
-                itemdata =  { 
-                    photo: {
-                        image: photo.media
-                    },
-                    template: "error",
-                }
-                data.push(itemdata);
-                $.imagespace.setItems(data);
+                var row = Titanium.UI.createTableViewRow({layout: "horizontal"});
+                var image = Titanium.UI.createImageView({
+                    image: photo.media,
+                    top: "5dp",
+                    left: "5dp",
+                    width: "100dp",
+                    left: 0
+                });
+
+                var label = Titanium.UI.createLabel({
+                    text: "Errore nel caricamento di quest'immagine, riprova più tardi."
+                });
+
+                row.add(image);
+                row.add(label);
+
+                $.imagespace.add(row);
+
                 $.activityIndicator.hide();
                 $.activityIndicator.height = 0;
             },
@@ -91,31 +158,30 @@ Array(images)[0].forEach(
     }
 );
 
-const DATEREGEX = /\d{2}\/\d{2}\/\d{4}/ // Regex del formato della data
-// Funzione invocata onBlur
-function saveText(e) {
-    fieldtext[e.source.id] = e.value;
-    if (e.source.id.startsWith("date") && !e.value.match(DATEREGEX)) {
-        alert("Attenzione: il formato della data dovrebbe essere gg/mm/aaaa come per esempio 11/05/2005. Altri formati potrebbero essere interpretati non correttamente, sebbene accettati.");
-    }
-}
-
-function bluronReturn(e) {
-    e.source.blur();
-}
+const DATEREGEX = /\d{2}\/\d{2}\/\d{4}/; // Regex del formato della data
 
 $.conferma.addEventListener("click", function(e){
+
     var photos = {};
     var campi_pieni = true;
 
-    uploaded.forEach(function(photo){
-        // Recupera da fieldtext i valori dei campi.
-        photos[photo.properties.itemId] = [fieldtext["title"+photo.properties.itemId], fieldtext["description"+photo.properties.itemId], fieldtext["date"+photo.properties.itemId]]
+    uploaded.forEach(function(id){
+        var title = fieldtext["title"+id].value.trim();
+        var description = fieldtext["description"+id].value.trim();
+        var date = fieldtext["date"+id].value.trim();
 
-        if (fieldtext["title"+photo.properties.itemId] == undefined || fieldtext["title"+photo.properties.itemId] == "" || fieldtext["description"+photo.properties.itemId] == undefined || fieldtext["description"+photo.properties.itemId] == "" || fieldtext["date"+photo.properties.itemId] == undefined || fieldtext["date"+photo.properties.itemId] == "") {
+        photos[id] = [title, description, date];
+
+        if (!date.match(DATEREGEX)) {
+            alert("Attenzione: il formato della data deve essere gg/mm/aaaa come per esempio 11/05/2005.");
+            campi_pieni = false;
+        }
+        if (title == "" || description == "" || date == "") {
             alert("I campi sono obbligatori! Compilali tutti prima di procedere all'upload (o premi invio sulla tastiera se l'hai fatto).");
             campi_pieni = false;
         }
+
+
     })
 
     if (campi_pieni) {
@@ -149,11 +215,7 @@ $.conferma.addEventListener("click", function(e){
 $.annulla.addEventListener("click", function(e){
     if(uploaded.length > 0) {
         $.activityIndicator.show();
-        var ids = [];
 
-        uploaded.forEach(function(photo){
-            ids.push(photo.properties.itemId);
-        })
         var url = Alloy.Globals.backend + "/photocancel.json";
         var client = Ti.Network.createHTTPClient({
             onload: function(e){
@@ -172,7 +234,7 @@ $.annulla.addEventListener("click", function(e){
         var content = {
             uuid: UUID,
             token: TOKEN,
-            ids: JSON.stringify(ids)
+            ids: JSON.stringify(uploaded)
         };
         client.send(content);
     } else {
