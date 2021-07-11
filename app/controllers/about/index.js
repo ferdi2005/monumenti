da9d5d00e472417857ab1d53a842f57f1fd3be71
ferdi2005 <ferdi.traversa@gmail.com>
2021-07-11 12:57:07 +0200

    Nascondo bottone elenco foto senza registrazione

--- app/controllers/about/index.js
+++ app/controllers/about/index.js
@@ -1,10 +1,26 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
-if (Ti.App.Properties.getBool("flurry", "notset") == true) {
-    $.tracciamento.value == true;
-} else {
-    $.tracciamento.value == false;
-}
+
+$.index.addEventListener("focus", function(e){
+    if (Ti.App.Properties.getBool("flurry", "notset") == true) {
+        $.tracciamento.value == true;
+    } else {
+        $.tracciamento.value == false;
+    }
+    
+    if (Ti.App.Properties.getBool("registrato", false) == false || Ti.App.Properties.getBool("autorizzato", false) == false) {
+        $.upload_list.hide();
+        if (OS_IOS) {
+            $.upload_list.height = 0;
+        }
+    } else {
+        $.upload_list.show();
+        if (OS_IOS) {
+            $.upload_list.height = Ti.UI.SIZE;
+        }
+    }
+});
+
 
 $.lbl_version.text = "Versione " + Ti.App.version;
 
@@ -13,12 +29,15 @@ $.info.addEventListener("click", function(e){
 });
 
 // Apri le impostazioni degli upload
+
 $.upload_config.addEventListener("click", function(e){
     Alloy.Globals.utils.open("upload/config", "settings");
 });
 
 $.upload_list.addEventListener("click", function(e){
-    Alloy.Globals.utils.open("upload/index");
+    if (Ti.App.Properties.getBool("registrato", false) == true && Ti.App.Properties.getBool("autorizzato", false) == true) {
+        Alloy.Globals.utils.open("upload/index");
+    }
 });
 
 $.tracciamento.addEventListener("change", function(e){
